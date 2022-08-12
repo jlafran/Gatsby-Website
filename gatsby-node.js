4bf776ebea6f4e3bf4c96662c9b00b19f0488401
FrancoToretta26 <68798898+FrancoToretta26@users.noreply.github.com>
2022-08-12 01:24:17 -0300

    Blogs Page

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,7 @@ exports.createPages = async ({ graphql, actions }) => {
   const postsPerPage = parseInt(process.env.GATSBY_POST_PER_PAGE) || 10;
   // templates path
   const singleBlogTemplate = require.resolve('./src/templates/single-blog.js');
+  const blogListTemplate = require.resolve('./src/templates/blog-list.js');
 
   const { createPage } = actions;
 
@@ -32,6 +33,20 @@ exports.createPages = async ({ graphql, actions }) => {
       path: `/blogs/${blog.node.frontmatter.slug}`,
       component: singleBlogTemplate,
       context: {slug: blog.node.frontmatter.slug}
+    });
+  });
+
+  const totalBlogListPages = Math.ceil(blogs.length / postsPerPage);
+  Array.from({ length: totalBlogListPages }).forEach((_, index) => {
+    createPage({
+      path: index === 0 ? `/blogs` : `/blogs/${index + 1}`,
+      component: blogListTemplate,
+      context: {
+        limit: postsPerPage,
+        offset: index * postsPerPage,
+        numberOfPages: totalBlogListPages,
+        currentPage: index + 1,
+      },
     });
   });
 
