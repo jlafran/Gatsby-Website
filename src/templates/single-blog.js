79e1906aa7091679e8019a9423e99fa9d9e28adf
FrancoToretta26 <68798898+FrancoToretta26@users.noreply.github.com>
2022-08-13 22:13:34 -0300

    Share buttons

--- src/templates/single-blog.js
+++ src/templates/single-blog.js
@@ -9,6 +9,43 @@ import ParagraphText from '../components/typography/ParagraphText';
 import { Title } from '../components/typography/Title';
 import { SingleBlogStyles } from '../styles/blog/SingleBlogStyles';
 import SEO from '../components/seo';
+import {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  TelegramShareButton,
+  WhatsappShareButton,
+  PinterestShareButton,
+  VKShareButton,
+  OKShareButton,
+  RedditShareButton,
+  TumblrShareButton,
+  LivejournalShareButton,
+  MailruShareButton,
+  ViberShareButton,
+  WorkplaceShareButton,
+  EmailShareButton,
+} from 'react-share';
+import {
+  FacebookIcon,
+  TwitterIcon,
+  TelegramIcon,
+  WhatsappIcon,
+  GooglePlusIcon,
+  LinkedinIcon,
+  PinterestIcon,
+  VKIcon,
+  OKIcon,
+  RedditIcon,
+  TumblrIcon,
+  LivejournalIcon,
+  MailruIcon,
+  ViberIcon,
+  WorkplaceIcon,
+  EmailIcon,
+} from 'react-share';
+
 
 export const postQuery = graphql`
   query SingleBlogQuery($slug: String!) {
@@ -22,6 +59,7 @@ export const postQuery = graphql`
         }
         description
         date
+        slug
         categoriesTitle
         categoriesSlug
       }
@@ -35,16 +73,15 @@ function SingleBlog({data}) {
   const texto = data.markdownRemark.html;
   return(
     <SingleBlogStyles>
-    <SEO title={blog.title} />
-    <PageSpace top={80} bottom={100}>
-      <div className="container">
-        <div className="blog-header">
-          <GatsbyImage
-            image={blog.featuredImage.childImageSharp.gatsbyImageData}
-            className="blog-cover-image"
-          />
-          <Title className="title">{blog.title}</Title>
-          <ParagraphText className="publishedAt">
+      <SEO title={blog.title} />
+      <PageSpace top={80} bottom={100}>
+        <div className="container">
+          <div className="blog-header">
+            <GatsbyImage
+              image={blog.featuredImage.childImageSharp.gatsbyImageData}
+              className="blog-cover-image" />
+            <Title className="title">{blog.title}</Title>
+            <ParagraphText className="publishedAt">
               <FiCalendar />
               {format(new Date(blog.date), 'p, MMMM dd, yyyy')}
             </ParagraphText>
@@ -52,13 +89,63 @@ function SingleBlog({data}) {
               <BiCategory />
               {blog.categoriesTitle}
             </ParagraphText>
-          <ParagraphText>
-          <div className='blog-content'
-            dangerouslySetInnerHTML={{__html: texto}} /></ParagraphText>
+            <ParagraphText>
+              <div className='blog-content'
+                dangerouslySetInnerHTML={{ __html: texto }} /></ParagraphText>
           </div>
+          <br/>
+          <br/>
+          <div>
+          <FacebookShareButton
+        url={"https://localhost:8000/blogs/"+blog.slug}
+        quote={"フェイスブックはタイトルが付けれるようです"}
+        hashtag={"#hashtag"}
+        description={"aiueo"}
+        className="Demo__some-network__share-button"
+      >
+        <FacebookIcon size={50} round /> 
+      </FacebookShareButton>
+      <TwitterShareButton
+        url={"https://localhost:8000/blogs/"+blog.slug}
+        quote={"フェイスブックはタイトルが付けれるようです"}
+        hashtag={"#hashtag"}
+        description={"aiueo"}
+        className="Demo__some-network__share-button"
+      >
+        <TwitterIcon size={50} round /> 
+      </TwitterShareButton>
+      <WhatsappShareButton
+        url={"https://localhost:8000/blogs/"+blog.slug}
+        quote={"フェイスブックはタイトルが付けれるようです"}
+        hashtag={"#hashtag"}
+        description={"aiueo"}
+        className="Demo__some-network__share-button"
+      >
+        <WhatsappIcon size={50} round />
+      </WhatsappShareButton>
+      <TelegramShareButton
+        url={"https://localhost:8000/blogs/"+blog.slug}
+        quote={"フェイスブックはタイトルが付けれるようです"}
+        hashtag={"#hashtag"}
+        description={"aiueo"}
+        className="Demo__some-network__share-button"
+      >
+        <TelegramIcon size={50} round />
+      </TelegramShareButton>
+      <EmailShareButton
+        url={"https://localhost:8000/blogs/"+blog.slug}
+        quote={"フェイスブックはタイトルが付けれるようです"}
+        hashtag={"#hashtag"}
+        description={"aiueo"}
+        className="Demo__some-network__share-button"
+      >
+        <EmailIcon size={10} round /> 
+      </EmailShareButton>
           </div>
-    </PageSpace>
-  </SingleBlogStyles>
+        </div>
+
+      </PageSpace>
+    </SingleBlogStyles>
   )
 
 }
